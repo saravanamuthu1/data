cc88e48a6b7b009332ce6503a084b0c6b00ea78a
saravanamuthu <saravanamuthu.97@gmail.com>
2022-03-08 19:31:34 -0600

    removed a functionality

--- main.py
+++ main.py
@@ -8,15 +8,11 @@ todrop=[]
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
      # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    dataframe.fillna(0)
-    dataframe.duplicated()
-    st.write(dataframe.describe())
+    df = pd.read_csv(uploaded_file)
+    df.fillna(0)
+    df.duplicated()
+    st.write(df.describe())
     st.write("colum name in the csv file")
-    for column in dataframe.columns:
+    for column in df.columns:
         st.markdown(column)
-    num_input=st.number_input("enter the number of colums to ve deleted",min_value=0,max_value=len(dataframe.columns),step=1)
-    for i in range(num_input):
-        text_input=st.text_input("enter the column to be deleted")
-        todrop.append(text_input)
-
+    text_input=st.text_input("enter the column to be deleted")
